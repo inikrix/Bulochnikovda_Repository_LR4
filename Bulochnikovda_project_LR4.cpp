36501c12d82c7a247d5af2b4509f271ac39d8bd9
inikrix <inikrix@gmail.com>
2025-05-21 14:11:23 +0300

    create project

---
+++ Bulochnikovda_project_LR4.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <map>
+#include <string>
+#include <functional>
+#include <cmath>
+
+using namespace std;
+
+int a;
+
+bool UserInput(string input) {
+    if(input.empty()) return false;
+
+    try {
+        int number = stoi(input);
+        if (number < 0) return false;
+    }
+    catch (...)
+    { return false; }
+    return true;
+}
+
+void enterNumber(int& varLink, string label){
+    string str_input;
+    cout << label << " = ";
+    getline (cin, str_input);
+
+    while (!UserInput(str_input)){
+        cout << label << " = ";
+        getline(cin, str_input);
+    }
+    varLink = stoi(str_input);
+}
+
+void enterA() {
+
+}
+
+void calcS3() {
+
+}
+
+void calcP3() {
+
+}
+
+struct MenuItem{
+    string title;
+    function<void()> action;
+};
+
+int main() {
+    map<int, MenuItem> menu = {
+        {1,{"ввести положительное число А", enterA}},
+        {2,{"вычислить площадь равностороннего треугольника S = а**2*Sqrt(3)/4", calcS3}},
+        {3,{"вычислить периметр равностороннего треугольника Р = 3*a", calcP3}},
+    };
+
+    int choice = 0;
+
+    while(true) {
+        cout << "Меню:" << endl;
+        for (const auto& item : menu){
+            cout << "Task " << item.first << ". " << item.second.title << endl; 
+        }
+        cout << "0. Выход" << endl;
+        enterNumber(choice, "Input number of menu: ");
+        if (choice == 0){
+            cout << "@ 2025 Bulochnikov DA" << endl;
+            break;
+        }
+        cout << endl;
+        if (menu.find(choice) != menu.end()){
+            menu[choice].action();
+            } else {
+                cout << "Некорректный ввод.";
+            }
+            cout << endl;
+    }
+    return 0;
+}
